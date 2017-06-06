00369a81dc0203a83ec283af9e77941acc6f193b
RafM <rkjmosiolek@gmail.com>
2017-06-06 12:28:34 +0100

    add jQuery DOM manipulation and new node in the AudioContext Interface

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,19 @@ window.onload = function(){
 	// Create new AudioContext.
 	var context = new AudioContext();
 
+
+	$(function() {
+		// jQuery selects each of the button elements and iterate over every one, calling the addAudioProperties() function on each.
+		$('#pads button').each(function() {
+			addAudioProperties(this);
+		});
+		// jQuery selects each of the button elements and listen for a click event. If the event occurs, calls the button element object’s play() method.
+		$('#pads button').click(function() {
+			this.play();
+
+		});
+	});
+
 	// Load audio files.
 	// Accept pad button element as an object.
 	// Accept the URL for the audio file.
@@ -28,17 +41,24 @@ window.onload = function(){
 	function addAudioProperties(object) {
 		// set the value for the name property matching the button id attribute.
 		object.name = object.id;
+		// set the source property to match the value of the HTML data-sound attribute.
 		object.source = $(object).data("sound");
+		// load the audio file to the buffer.
 		loadAudio(object, object.source);
+		
 		object.play = function() {
+			// place a new node in the AudioContext Interface.
 			var s = context.createBufferSource();
+			// set the node's source property.
 			s.buffer = object.buffer;
+			// connect the audio source to system's default audio output (usually speakers or headphones).
 			s.connect(context.destination);
+			// play the sound.
 			s.start(0);
+			// attach the audio source to the pad object's s property.
 			object.s = s;
 		};
 	}
-
 
 
 
